22ac4bbfbb5656b3948fe4d479f4692044dc5dd2
Reinhard Simon <rsimon64@gmail.com>
2015-01-29 10:53:04 -0500

    more on #!

--- R/demo.R
+++ R/demo.R
@@ -1,9 +1,11 @@
-#' demo
+#' @name  demo
 #' 
-#' A dummy demo function
+#' @description A dummy demo function
+#' 
 #' @author Reinhard SImon
-#' @example /inst/example/demo.R
+#' @example /inst/examples/demo.R
 #' @return string hello
+#' @aliases demo
 #' @export
 demo <- function(){
   "Hello!"
--- R/package.R
+++ R/package.R
@@ -1,6 +1,6 @@
-#' ciptools
-#' Tools to further standardize R development.
+#' @title ciptools
+#' @name ciptools
+#' @description Tools to further standardize R development.
 #' @docType package
 #' @author Reinhard Simon
-#' @export
 NULL
