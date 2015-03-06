19bef50c76aa5f08fae5b4df1fc9b561fd023eab
c5sire <rsimon64@gmail.com>
2015-03-05 22:00:48 -0500

    Improved template tests.

--- inst/templates/test_hello.R
+++ inst/templates/test_hello.R
@@ -3,7 +3,7 @@ context("hello")
 test_that("hello function operates well", {
   # The following function should be used to reduce time on CRAN
   # by avoiding running a test since time on CRAN is limited.
-  skip_on_cran()
+  # skip_on_cran()
 
   expect_that(hello() == "Hello, world!", is_true())
   expect_that(hello(name = "Reinhard") == "Hello, Reinhard!", is_true())
