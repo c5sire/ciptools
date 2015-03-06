03ed3c3698ff1505ed42fa9f087b4659cb78280a
c5sire <rsimon64@gmail.com>
2015-03-05 20:46:32 -0500

    added a build function to help with vignettes

--- R/build.R
+++ R/build.R
@@ -1,3 +1,11 @@
+do_knit <- function(afile, adir) {
+  devtools::in_dir(
+    adir,
+    rmarkdown::render("tutorial.Rmd", "html_vignette")  
+  )
+  
+}
+
 #' build
 #' 
 #' Takes special care of vignettes
@@ -5,6 +13,21 @@
 #' @author Reinhard Simon
 #' @family
 #' @export
-build <- function(aname){
+build <- function(){
+  # build vignettes
+  vig_lst <- list.files("vignettes", pattern = "*.Rmd")
+  sapply(vig_lst, do_knit, "vignettes")
+  # copy over to inst/doc
+  fls_lst <- file.path("vignettes",list.files("vignettes"))
+  if(!file.exists("inst/doc")) {
+    dir.create("inst/doc")
+  }
+  file.copy(fls_lst, file.path("inst", "doc"), over = TRUE, rec = TRUE)
+
+  # devtools build
+  devtools::build()
+  
+  # format
+  format_code()
   
 }
