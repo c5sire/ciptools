2a2a8c2d916034911017a618d4433daaf128f504
Alexander Vowinkel <vowinkel.alexander@gmail.com>
2015-04-22 13:34:46 -0500

    Revert "Revert "remedied unintended automated formatting""
    This reverts commit 73a7a6c5f49b210649498bdb3abcd5d6bc757353.

--- inst/templates/tpl_tst.R
+++ inst/templates/tpl_tst.R
@@ -4,9 +4,5 @@ test_that("{{title}} function operates well", {
   # The following function should be used to reduce time on CRAN by avoiding running a test since time on CRAN is limited.
   # skip_on_cran()
   
-  expect_that({
-    {
-      title
-    }
-  }() == "Hello, world!", is_true())
+  expect_that({{title}}() == "Hello, world!", is_true())
 }) 
